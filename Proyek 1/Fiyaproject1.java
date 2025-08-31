6c487eb7a97174730454f897de4dc8a33adad755
Nofiya Millatina <nofiyamillatina15@gmail.com>
2025-08-31 15:20:51 +0700

    Add files via upload

---
+++ Proyek 1/Fiyaproject1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fiyaproject1;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Fiyaproject1 {
+
+    public static void main(String[] args) {
+        String Nama = "Nofiya Millatina";
+        String NPM = "2420506026";
+        String Alamat = "Kauman, Grabag, Magelang";
+        String No_hp = "0882003809375";
+        
+        System.out.println("Nama    : " + Nama);
+        System.out.println("NPM     : " + NPM);
+        System.out.println("Alamat  : " + Alamat);
+        System.out.println("No Hp   : " + No_hp);
+    }
+}
